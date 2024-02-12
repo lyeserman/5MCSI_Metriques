f065e8b1d95fc6d3ce4ff558bafded9c670e15d8
lyeserman <159757774+lyeserman@users.noreply.github.com>
2024-02-12 09:01:23 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,17 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template_string, render_template, jsonify
 from flask import render_template
 from flask import json
+from datetime import datetime
+from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return render_template('hello.html')
-
-@app.route('/fr/')
-def hello_world_fr():
-    return "<h2>Bonjour tout le monde !</h2>"
-                                                                                                                                       
+    return render_template('hello.html') #Comm
+  
 if __name__ == "__main__":
   app.run(debug=True)
